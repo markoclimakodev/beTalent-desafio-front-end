632b2bfde800feb74577f86f68372c58478ed43c
markoclimako <mkclimako@gmail.com>
2024-07-15 20:43:39 -0300

    refactor: simplify useEmployees hook for employee data fetching
    - Update variable names and remove redundant state for filtered employees.
    - Add loading state simulation.

--- src/hooks/useEmployees.ts
+++ src/hooks/useEmployees.ts
@@ -1,43 +1,46 @@
 import { useState, useEffect } from "react";
 import { Employee } from "../types/Employee";
 
-export const useEmployees = (apiUrl: string = 'http://localhost:3000/employees') => {
-    const [employee, setEmployee] = useState<Employee[]>([]);
-    const [filteredEmployees, setFilteredEmployees] = useState<Employee[]>([]);
+export const useEmployees = () => {
+    const API_URL = 'http://localhost:3000/employees'
+    const [employees, setEmployees] = useState<Employee[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-  
+
     useEffect(() => {
-      let isMounted = true; 
-  
-      const fetchData = async () => {
-        try {
-          const response = await fetch(apiUrl);
-          if (!response.ok) {
-            throw new Error('Erro ao carregar usuários');
-          }
-          const data: Employee[] = await response.json();
-          if (isMounted) {
-            setEmployee(data);
-            setFilteredEmployees(data);
-            setLoading(false);
-          }
-        } catch (err) {
-          if (isMounted && err instanceof Error) {
-            setError(err.message);
-            setLoading(false);
-          }
-        }
-      };
-  
-      fetchData();
-  
-      return () => {
-        isMounted = false;
-      };
-    }, [apiUrl]);
-  
-    return { employee, filteredEmployees, setFilteredEmployees, loading, error };
-  };
-  
-  
+        let isComponentMounted = true;
+
+        const fetchEmployees = async () => {
+            try {
+                // Simulação de espera de 1 segundo para demonstrar o estado de carregamento da aplicação
+                await new Promise(resolve => setTimeout(resolve, 1000));
+
+                const response = await fetch(API_URL);
+                if (!response.ok) {
+                    throw new Error('Erro ao carregar funcionários');
+                }
+                const employeeData: Employee[] = await response.json();
+                if (isComponentMounted) {
+                    setEmployees(employeeData);
+                    setLoading(false);
+                }
+            } catch (error) {
+                if (isComponentMounted && error instanceof Error) {
+                    setError(error.message);
+                }
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+
+        fetchEmployees();
+
+        return () => {
+            isComponentMounted = false;
+        };
+    }, [API_URL]);
+
+    return { employees, loading, error };
+};
+
